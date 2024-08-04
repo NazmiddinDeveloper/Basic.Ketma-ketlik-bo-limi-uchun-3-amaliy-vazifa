bf39977ec8acf00d75b41043dacf297f9f4fe963
nazmiddin <nazmiddinsirojiddinov080@gmail.com>
2024-08-04 20:36:13 +0500

    1.Tub sonlar:

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// Basic.Ketma-ketlik bo'limi uchun 3-amaliy vazifa
+﻿using System;
+// Basic.Ketma-ketlik bo'limi uchun 3-amaliy vazifa
 // 1.Tub sonlar:
+
+Console.Write(" Output :");
+string nStr = Console.ReadLine();
+int n = Convert.ToInt32(nStr);
+
+int x = 1;
+
+for (int i = 2; i < n; i++)
+{
+    if (n%i == 0)
+    {
+        x++;
+    }
+}
+
+if(x == 1){
+    Console.WriteLine(" Output = \"TUB\"");
+}else{
+    Console.WriteLine(" Output = \"MURAKKAB\"");
+}
