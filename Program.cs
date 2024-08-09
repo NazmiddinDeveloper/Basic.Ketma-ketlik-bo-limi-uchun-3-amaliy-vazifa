ba69da0d8e20d3a36630399eacec1e1aa1cf0c40
nazmiddin <nazmiddinsirojiddinov080@gmail.com>
2024-08-09 23:33:52 +0500

    Daraja hisoblash ok boldi

--- Program.cs
+++ Program.cs
@@ -1,2 +1,33 @@
-﻿// Basic.Ketma-ketlik bo'limi uchun 3-amaliy vazifa
-// 1.Tub sonlar:
+﻿
+// 3.Daraja hisoblash:
+Console.Write($" Son kiriting : ");
+string sonStr = Console.ReadLine();
+int son = Convert.ToInt32(sonStr);
+
+
+long sonP;
+while (true){
+    Console.Write($" {son} ni nechanchi darajaga ko'tarmoqchisiz : ");
+    string sonStrP = Console.ReadLine();
+    bool a = false;
+    for(int z = 0; z < sonStrP.Length; z++){
+        if(sonStrP[z] == '.'){
+            a = true;
+            break;
+        }
+    }
+    if(a == false && Convert.ToInt64(sonStrP) > 0){
+        sonP = Convert.ToInt64(sonStrP);
+        break;
+    }
+}
+
+int result = son;
+for (int i = 1; i < sonP; i++)
+{
+    result *= son;
+}
+System.Console.WriteLine($"  Output = {result}");
+
+
+
