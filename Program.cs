7f7b143604952154d14007461a7b40867dacdb09
Nazmiddin <153126910+NazmiddinDeveloper@users.noreply.github.com>
2024-08-25 16:48:33 +0500

    Update Program.cs
    0.0.1 COMMENTS

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// Basic.Ketma-ketlik bo'limi uchun 3-amaliy vazifa
+﻿// -- Basic.Ketma-ketlik bo'limi uchun 3-amaliy vazifa
+/* 
+HAMMA VAZIFALAR JAMLANMASI USHUBU MASTER BRANCHGA PR QILINGAN 
+VA ALOHIDA ALOHIDA USHBU REPO BRANCHLARIDAHAM TOPISH MUMKIN 
+*/
+
 // 1.Tub sonlar:
+// 2.Bo'luvchisiz sonlar: 
+// 3.Daraja hisoblash:
+// 4.Harajatlar to'plamini hisoblash:
+// 5.Armstrong sonlar:
+// 6.Sifatli bo'luvchilar:
+
